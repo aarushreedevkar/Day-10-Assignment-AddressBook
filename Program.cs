c99f06f9275d774e0f09a03e23c21b618db69cb1
jyoti <jyotidhage10@gmail.com>
2022-08-14 18:12:05 +0530

    Welcome to AddressBook Program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AddressBook2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program ");
+            Console.WriteLine("-------------Welcome to the Address Book Orogram------------ ");
+            Person person = new Person("Aaradhya", "Devkar", "Hadapsar", "pune", "maharashtra", "412308", "9673683178", "jyotidhage10@gmail.com");
+            Console.WriteLine(person.getName());
+            Console.WriteLine(person.getlastName());
+            Console.WriteLine(person.getaddress());
+            Console.WriteLine(person.getcity());
+            Console.WriteLine(person.getstate());
+            Console.WriteLine(person.getzip());
+            Console.WriteLine(person.getphone());
+            Console.WriteLine(person.getemail());
+            Console.Read();
+        }
+    }
+}
